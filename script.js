df77f5c1e7eec97b56ed346891a9c31442adfde2
ilovecatsquiet <156492106+ilovecatsquiet@users.noreply.github.com>
2025-02-16 17:07:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -26,8 +26,10 @@ window.onclick = function(event) {
     }
 };
 
-// Sidebar Toggle Functionality
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
+    const body = document.body;
+
     sidebar.classList.toggle("active");
+    body.classList.toggle("sidebar-open");
 }
